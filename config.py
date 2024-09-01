8d5432e3544aadd7271a843e2b2be1eb2e2c59d6
CY <111054799+GohChunYen@users.noreply.github.com>
2024-09-01 19:54:29 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cbfqxmqdnhxf.us-east-1.rds.amazonaws.comemployee.cbfqxmqdnhxf.us-east-1.rds.amazonaws.com
+customuser = "aws_user"
+custompass = "password"
+customdb = "employee"
+custombucket = "gcy-employee"
+customregion = "us-east-1"
+
